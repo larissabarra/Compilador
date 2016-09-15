dcce616d1dc29494ccb334a32fe0e912137905f6
Larissa Barra <lari_conde94@hotmail.com>
2016-09-14 21:54:46 -0300

    Acrescentadas classes para a tabela de símbolos

---
+++ src/compilador/Env.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilador;
+
+import java.util.Hashtable;
+
+/**
+ *
+ * @author Larissa
+ */
+public class Env {
+
+    private Hashtable table; //tabela de símbolos do ambiente
+    protected Env prev; //ambiente imediatamente superior
+
+    public Env(Env n) {
+        table = new Hashtable(); //cria a TS para o ambiente
+        prev = n; //associa o ambiente atual ao anterior
+    }
+    /*Este método insere uma entrada na TS do ambiente */
+    /*A chave da entrada é o Token devolvido pelo analisador léxico */
+    /*Id é uma classe que representa os dados a serem armazenados na TS para */
+    /*identificadores */
+
+    public void put(Token w, Id i) {
+        table.put(w, i);
+    }
+    /*Este método retorna as informações (Id) referentes a determinado Token */
+    /*O Token é pesquisado do ambiente atual para os anteriores */
+
+    public Id get(Token w) {
+        for (Env e = this; e != null; e = e.prev) {
+            Id found = (Id) e.table.get(w);
+            if (found != null) //se Token existir em uma das TS
+            {
+                return found;
+            }
+        }
+        return null; //caso Token não exista em uma das TS
+    }
+}
---
+++ src/compilador/Id.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilador;
+
+/**
+ *
+ * @author Larissa
+ */
+public class Id {
+    private String name; //puxo uma word?
+    private int type; //vem da tag
+    private String bloco; //vai ter?
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getBloco() {
+        return bloco;
+    }
+
+    public void setBloco(String bloco) {
+        this.bloco = bloco;
+    }
+    
+}
